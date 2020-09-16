4d9b70013ee4dd14d684783ab38babc1f6bf9376
Bruna Rosa <brosa@bionexo.com>
2020-09-16 00:07:19 -0300

    Add new class - valid age invalid

--- desafios/hello/hello_age_invalid.rb
+++ desafios/hello/hello_age_invalid.rb
@@ -6,6 +6,8 @@ require 'active_support/core_ext'
 def hello
   puts 'Informe seu nome: '
   @nome = gets.chomp.capitalize # converte a primeira leta
+  puts 'Informe sua idade:'
+  @idade = gets.chomp.to_i # converte pra inteiro a 'string'
 end
 
 def valid_name
@@ -16,33 +18,24 @@ def valid_name
   end
 end
 
-def invalid_age
-  puts 'Informe sua idade:'
-  @idade = gets.chomp.to_i # converte pra inteiro a 'string'
-  loop do
-    @idade = gets.chomp
-    break if is_numeric?(@idade)
-    puts "O valor que você inseriu não é válido"
-  end
-    msg_hello
-end
 
 def msg_hello 
   @valid_name = "#{@nome}"
 
-  if @idade < 12
-    puts "Oxi, vai brincar na rua! Bem-vindo, #{valid_name}!"
-  elsif @idade >= 12 && @idade <= 18
-    puts "Salve, salve família! Bem-vindo, #{valid_name}!"
-  elsif @idade > 18 && @idade <= 30
-    puts "Vida de adulto não é fácil! Bem-vindo, #{valid_name}!"
-  elsif @idade > 30 && @idade <= 50
-    puts "A vida é pagar boleto e tentar emagrecer! Bem-vindo, #{valid_name}!"
-  elsif @idade > 50
-    puts "Ficar velho é sorte! Bem-vindo, #{valid_name}!"
-  end
+    while @idade.present?
+      puts "Digite sua idade novamente"
+    break if @idade < 12
+      puts "Oxi, vai brincar na rua! Bem-vindo, #{@valid_name}!"
+    break if @idade >= 12 && @idade <= 18
+      puts "Salve, salve família! Bem-vindo, #{@valid_name}!"
+    break if @idade > 18 && @idade <= 30
+      puts "Vida de adulto não é fácil! Bem-vindo, #{@valid_name}!"
+    break if @idade > 30 && @idade <= 50
+      puts "A vida é pagar boleto e tentar emagrecer! Bem-vindo, #{@valid_name}!"
+    break if @idade > 50
+      puts "Ficar velho é sorte! Bem-vindo, #{@valid_name}!"
+    end
 end
 
 hello
-valid_name
-invalid_age
+msg_hello
