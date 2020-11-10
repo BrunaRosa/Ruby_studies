8e94b744ce6f63d6b0f3a223c1f014fd78614fb9
BrunaRosa <brosa@bionexo.com>
2020-11-09 23:54:33 -0300

    Create lessons methods/atributtes/constructors

---
+++ ruby_oo/definindo_metodos.rb
@@ -0,0 +1,20 @@
+class Pessoa
+
+    def gritar(texto = "Grrrhhhhhh!")
+        puts "Grintando .... #{texto}"
+    end
+
+    def falar
+        puts "oláaa!"
+    end
+
+    def agradecer
+        puts "Obrigada!"
+    end
+
+end
+
+objeto = Pessoa.new
+objeto.gritar("Aooooooooo!")
+objeto.falar
+objeto.agradecer
